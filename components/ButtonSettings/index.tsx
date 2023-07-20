8f8efbc1553f83fa5f7d16d6daa8dd2b85e7f67b
David-Clayton <david@lionsandlegacy.com>
2023-07-20 15:28:54 -0700

    feat: livestream integration

--- components/ButtonSettings/index.tsx
+++ components/ButtonSettings/index.tsx
@@ -5,13 +5,20 @@ type Props = {
   width?: number;
   onClick: Function;
   bgColor?: string;
+  disabled?: boolean;
 };
 
-const ButtonSettings = ({ label, onClick, bgColor }: Props) => {
+const ButtonSettings = ({
+  label,
+  onClick,
+  bgColor,
+  disabled = false,
+}: Props) => {
   return (
     <button
+      disabled={disabled}
       className={twMerge(
-        "w-full inline-flex font-sans justify-center items-center text-primary text-lg p-2 rounded-md transition-all duration-300 cursor-pointer",
+        "w-full inline-flex font-sans justify-center items-center text-primary text-lg p-2 rounded-md transition-all duration-300 cursor-pointer disabled:bg-gray-700 disabled:text-secondary",
         bgColor
           ? "bg-bluePrimary hover:bg-blueSecondary"
           : "bg-[#dc3545] hover:bg-[#ff5061]"
